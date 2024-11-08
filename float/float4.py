14a234a674050b19312259af369f5c97f6924296
Odina <166241854+Ok1738@users.noreply.github.com>
2024-11-08 21:26:00 +0500

    Add files via upload

---
+++ float/float4.py
@@ -0,0 +1,5 @@
+import math
+x=14.26
+y= -1.22
+p=3.14
+print(str((2*math.cos(x-p/6))/(0.5+pow(math.sin(y),2))))
---
+++ float/float5.py
@@ -0,0 +1,4 @@
+import math
+x=14.26
+z=3.5*pow(10,-2)
+print(str(abs(x-z*x)+(pow(z,2)/((3-pow(z,2))/-5))))
